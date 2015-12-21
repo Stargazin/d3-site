563afdc178035d30665921d5c35ba1e6aca58885
Stargazin <alexchihsun@yahoo.com>
2015-12-20 18:09:07 -0800

    changes to settings for deployment

--- d3/config/settings/local.py
+++ d3/config/settings/local.py
@@ -14,18 +14,6 @@ DATABASES = {
 	}
 }
 
-
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# 		'NAME': 'd3',
-# 		'USER': 'd3',
-# 		'PASSWORD': 'd3',
-# 		'HOST': 'localhost',
-# 		'PORT': '',
-# 	}
-# }
-
 STATIC_URL = '/assets/'
 
 INSTALLED_APPS += ()
