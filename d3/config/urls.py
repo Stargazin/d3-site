2ea0235b844ecdeb3b767359ef2c36bff0546c75
Stargazin <alexchihsun@yahoo.com>
2015-12-23 18:23:32 -0800

    changed home url temporarily; added google analytics script

--- d3/config/urls.py
+++ d3/config/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+# from core.views import IndexView
+from legendaries.views import AllLegendariesView
+
 
 urlpatterns = [
+    url(r'^$', AllLegendariesView.as_view(), name='index'),
     url(r'^administrator/', include(admin.site.urls)),
     url(r'^legendaries/', include('legendaries.urls')),
     url(r'^sets/', include('itemsets.urls')),
